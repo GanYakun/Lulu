c2e656d65f645c0d91fd50ec6acb74be25e68587
GanYakun <1772894420@qq.com>
2023-12-26 12:02:01 +0800

    add package

---
+++ src/main/java/com/example/demo/Controller/RequestController.java
@@ -0,0 +1,12 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestController {
+    @RequestMapping("hello")
+    String hello(){
+        return "Hello World!";
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,15 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-    @RequestMapping("hello")
-    String hello(){
-        return "Hello World!";
-    }
+
 }
